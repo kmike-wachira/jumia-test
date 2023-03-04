20af91090d5acc6caf9401ee96ac5ce94a7e9a9e
Kmike <44137594+kmike-wachira@users.noreply.github.com>
2023-03-04 15:52:10 +0300

    adding axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 const app = createApp(App)
 app.use(router)
+app.use(axios)
 app.mount('#app')
