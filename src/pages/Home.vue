a7fd64ff4d06386590f7e30c31a82731a53bfb5d
Kmike <44137594+kmike-wachira@users.noreply.github.com>
2023-03-04 11:56:43 +0300

    menu

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,20 +1,21 @@
 <template>
-  <div>
+  <div class="z-[10]">
     <navbar-vue />
   </div>
-  <main>
-    <section class="flex">
-      <div>menu</div>
-      <div>carousel</div>
+  <main class="z-[0] pt-2">
+    <section class="flex h-full">
+      <div class="flex-none"><menu-vue /></div>
+      <div class="flex-1 text-center">carousel</div>
     </section>
     <section>products listing</section>
   </main>
 </template>
 
 <script>
+import MenuVue from "../components/Menu.vue";
 import NavbarVue from "../components/Navbar.vue";
 export default {
-  components: { NavbarVue },
+  components: { NavbarVue, MenuVue },
 };
 </script>
 
