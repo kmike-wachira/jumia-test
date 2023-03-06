aa58d54a29576a5b23af71fb7ad68188e9f8c21a
Kmike <44137594+kmike-wachira@users.noreply.github.com>
2023-03-06 23:11:34 +0300

    categories listing ui

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,94 @@
+<template>
+  <section class="m-3 card shadow-md rounded-xl">
+    <div class="text-1xl font-semibold w-full bg-red-200 p-2">Categories</div>
+
+    <div class="carousel rounded-box m-3">
+      <div
+        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+      >
+        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
+          Laptops
+        </div>
+      </div>
+      <div
+        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+      >
+        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
+          Laptops
+        </div>
+      </div>
+      <div
+        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+      >
+        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
+          Laptops
+        </div>
+      </div>
+      <div
+        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+      >
+        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
+          Laptops
+        </div>
+      </div>
+      <div
+        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+      >
+        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
+          Laptops
+        </div>
+      </div>
+      <div
+        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+      >
+        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
+          Laptops
+        </div>
+      </div>
+      <div
+        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+      >
+        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
+          Laptops
+        </div>
+      </div>
+      <div
+        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+      >
+        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
+          Laptops
+        </div>
+      </div>
+      <div
+        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+      >
+        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
+          Laptops
+        </div>
+      </div>
+      <div
+        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+      >
+        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
+          Laptops
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,16 +3,30 @@
     <navbar-vue />
   </div>
   <main class="z-[10] pt-2">
-    <section class="flex mt-16 gap-2 h-[100%]">
+    <section class="flex mt-16 gap-2 h-[100%] md:mx-4">
       <div class="flex-none z-0 hidden md:block">
         <menu-vue :categories="categories" />
       </div>
       <div class="flex-1 text-center h-[100%] bg-red-400 h-full">
-        <div class="flex container justify-center">
+        <div class="flex container justify-center h-[70%] shadow-sm">
           <img src="../assets/laptop.png" alt="" class="" srcset="" />
         </div>
       </div>
+      <div class="flex-none z-0 hidden md:block w-64 my-4 w-100">
+        <div class="flex flex-col justify-between gap-2">
+          <div class="card shadow-sm p-2 rounded-sm shadow-sm">
+            1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi repellat
+            dicta numquam illo voluptatibus dignissimos corrupti porro distinctio quos.
+            Nesciunt alias ab sunt fugit nemo nostrum quibusdam labore earum eligendi.
+          </div>
+          <div class="card shadow-sm p-2 rounded-sm bg-red-400">
+            <img src="../assets/laptop.png" alt="" class="" srcset="" />
+          </div>
+        </div>
+      </div>
     </section>
+    <!-- categories section -->
+    <category-list></category-list>
     <section class="z-0">
       <productlisting-vue />
     </section>
@@ -23,9 +37,10 @@
 import axios from "axios";
 import MenuVue from "../components/Menu.vue";
 import NavbarVue from "../components/Navbar.vue";
+import CategoryList from "../components/CategoryList.vue";
 import ProductlistingVue from "../components/Productlisting.vue";
 export default {
-  components: { NavbarVue, MenuVue, ProductlistingVue },
+  components: { NavbarVue, MenuVue, ProductlistingVue, CategoryList },
   data() {
     return {
       categories: [],
