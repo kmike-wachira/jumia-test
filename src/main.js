e51cbb22cc6c48c5e8ddb43e588fd18f747dc2a5
Kmike <44137594+kmike-wachira@users.noreply.github.com>
2023-03-06 10:53:28 +0300

    adding pinia

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 // import axios from 'axios'
 // import VueAxios from 'vue-axios'
 const app = createApp(App)
+const pinia = createPinia()
+
 // app.use(VueAxios, axios)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
