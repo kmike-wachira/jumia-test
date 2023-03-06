ac25bd3da6fe11002a175c22192a7b94304ec232
Kmike <44137594+kmike-wachira@users.noreply.github.com>
2023-03-06 23:21:08 +0300

    category list

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,84 +1,86 @@
 <template>
   <section class="m-3 card shadow-md rounded-xl">
-    <div class="text-1xl font-semibold w-full bg-red-200 p-2">Categories</div>
+    <div class="text-1xl font-semibold w-full bg-red-200 p-2 ont-mono border-l-4">
+      Categories
+    </div>
 
     <div class="carousel rounded-box m-3">
       <div
-        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+        class="carousel-item flex flex-col mx-1 p-2 rounded-xl hover:shadow-xl transition-all"
       >
-        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <img src="../assets/laptop.png" width="120" class="shadow-sm" alt="Drink" />
         <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
           Laptops
         </div>
       </div>
       <div
-        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+        class="carousel-item flex flex-col mx-1 p-2 rounded-xl hover:shadow-xl transition-all"
       >
-        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <img src="../assets/laptop.png" width="120" class="shadow-sm" alt="Drink" />
         <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
           Laptops
         </div>
       </div>
       <div
-        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+        class="carousel-item flex flex-col mx-1 p-2 rounded-xl hover:shadow-xl transition-all"
       >
-        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <img src="../assets/laptop.png" width="120" class="shadow-sm" alt="Drink" />
         <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
           Laptops
         </div>
       </div>
       <div
-        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+        class="carousel-item flex flex-col mx-1 p-2 rounded-xl hover:shadow-xl transition-all"
       >
-        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <img src="../assets/laptop.png" width="120" class="shadow-sm" alt="Drink" />
         <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
           Laptops
         </div>
       </div>
       <div
-        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+        class="carousel-item flex flex-col mx-1 p-2 rounded-xl hover:shadow-xl transition-all"
       >
-        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <img src="../assets/laptop.png" width="120" class="shadow-sm" alt="Drink" />
         <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
           Laptops
         </div>
       </div>
       <div
-        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+        class="carousel-item flex flex-col mx-1 p-2 rounded-xl hover:shadow-xl transition-all"
       >
-        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <img src="../assets/laptop.png" width="120" class="shadow-sm" alt="Drink" />
         <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
           Laptops
         </div>
       </div>
       <div
-        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+        class="carousel-item flex flex-col mx-1 p-2 rounded-xl hover:shadow-xl transition-all"
       >
-        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <img src="../assets/laptop.png" width="120" class="shadow-sm" alt="Drink" />
         <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
           Laptops
         </div>
       </div>
       <div
-        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+        class="carousel-item flex flex-col mx-1 p-2 rounded-xl hover:shadow-xl transition-all"
       >
-        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <img src="../assets/laptop.png" width="120" class="shadow-sm" alt="Drink" />
         <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
           Laptops
         </div>
       </div>
       <div
-        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+        class="carousel-item flex flex-col mx-1 p-2 rounded-xl hover:shadow-xl transition-all"
       >
-        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <img src="../assets/laptop.png" width="120" class="shadow-sm" alt="Drink" />
         <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
           Laptops
         </div>
       </div>
       <div
-        class="carousel-item flex flex-col mx-2 border p-2 rounded-xl hover:shadow-xl transition-all"
+        class="carousel-item flex flex-col mx-1 p-2 rounded-xl hover:shadow-xl transition-all"
       >
-        <img src="../assets/laptop.png" width="100" class="shadow-sm" alt="Drink" />
+        <img src="../assets/laptop.png" width="120" class="shadow-sm" alt="Drink" />
         <div class="text-1xl font-semibold w-full bg-gray-200 p-1 text-center">
           Laptops
         </div>
